e529a145aee624a97747bc48cda63351eda4b915
Jason Osteen <jtvmo4@gmail.com>
2020-06-23 14:35:17 -0700

    push to turn in

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-// Current Day Display 
-$("#currentDay").text(moment().format('dddd,MMMM Do'));
+// Displays the day of the week in header
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-// Time Display 
-var Timer = function () { 
-    $("#currentTimer").text(moment().format('hh:mm:ss A'));
+// Dynamically changing live time at top of page
+var updateTime = function () {
+  $("#currentClock").text(moment().format('hh:mm:ss A'));
 }
-setInterval(Timer, 1000);
-Timer();
+setInterval(updateTime, 1000);
+updateTime();
 
+// Assigning hours 9am-5pm in 24hr format
+const hoursInDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// Formats time into 24hr and into a number vs string
+let currentTime = parseInt(moment().format('HH'));
+
+// Determines color of timeslot based on the time of day
+function timeSlot() {
+  for (let i = 0; i < hoursInDay.length; i++) {
+    if (hoursInDay[i] > currentTime) {
+      $(`#${hoursInDay[i]}`).addClass("future");
+    } else if (hoursInDay[i] === currentTime) {
+      $(`#${hoursInDay[i]}`).addClass("present");
+    } else {
+      $(`#${hoursInDay[i]}`).addClass("past");
+    }
+  }
+  return;
+};
+timeSlot();
+
+// Saves current task inside the text box
+$(this).on("click", function (event) {
+  event.preventDefault();
+  for (let i = 0; i < hoursInDay.length; i++) {
+    let currentTask = $(`#task${hoursInDay[i]}`).val();
+    localStorage.setItem(`save${hoursInDay[i]}`, currentTask);
+  }
+});
+
+// Shows previously saved tasks in the task input box
+function showTasks() {
+  for (let i = 0; i < hoursInDay.length; i++) {
+    let displayTask = localStorage.getItem(`save${hoursInDay[i]}`);
+    $(`#task${hoursInDay[i]}`).val(displayTask);
+  }
+};
+showTasks();
